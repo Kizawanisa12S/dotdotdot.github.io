74c1444b596d681ce114268907fa07fc725a03f3
dotdotdot <155509749+Kizawanisa12S@users.noreply.github.com>
2024-01-16 11:27:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
+<!-- index.php -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Simple PHP Page</title>
 </head>
 <body>
-    <?php 
-        echo "123123123123";
-    php>
+    <h1>Hello, this is a simple PHP page!</h1>
+    <?php
+        $message = "Welcome to my PHP page!";
+        echo "<p>$message</p>";
+    ?>
 </body>
 </html>
